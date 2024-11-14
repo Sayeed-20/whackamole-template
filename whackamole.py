d0f24bbd3cc5f2f6f76e67d6ba2b5c5305a9ea3d
Sayeed Azam <sayeed.azam838@gmail.com>
2024-11-14 15:12:12 -0500

    first commit

--- whackamole.py
+++ whackamole.py
@@ -1,4 +1,11 @@
 import pygame
+import random
+#test comment
+#constants
+sq_size = 32
+screen_width = 640
+screen_height = 512
+
 
 
 def main():
@@ -7,16 +14,29 @@ def main():
         # You can draw the mole with this snippet:
         # screen.blit(mole_image, mole_image.get_rect(topleft=(x,y)))
         mole_image = pygame.image.load("mole.png")
-        screen = pygame.display.set_mode((640, 512))
+        screen = pygame.display.set_mode((screen_width, screen_height))
         clock = pygame.time.Clock()
         running = True
+        a = 2.5
+        b = 2.5
         while running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
             screen.fill("light green")
+            for i in range(1, sq_size):
+                pygame.draw.line(screen, (255, 0, 0), (i*sq_size, 0), (i*sq_size, 512))
+                pygame.draw.line(screen, (255, 0, 0), (0, i * sq_size), (640, i * sq_size))
+            screen.blit(mole_image, mole_image.get_rect(topleft=(a, b)))
             pygame.display.flip()
             clock.tick(60)
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                x, y = event.pos
+                if (x // 32 == a // 32) and (y // 32 == b // 32):
+                    a = random.randrange(0, screen_width - sq_size, sq_size) + 2.5
+                    b = random.randrange(0, screen_height - sq_size, sq_size) + 2.5
+
+
     finally:
         pygame.quit()
 
